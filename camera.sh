e49cc5449a7397fe09397bdba78819e64272c233
Milton Abrunhosa <ml_byteman@yahoo.com.br>
2021-09-05 15:28:00 -0300

    exec directory

--- camera.sh
+++ camera.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # change dir
-cd ~/Public/hdw/camera.timelapse
+cd ~/Public/hdw/camera.timelapse/02.git/time_lapsed
 
 # make 8 hours movie (8*60 = 480 min)
 python3 make_tlmov.py -d 480
